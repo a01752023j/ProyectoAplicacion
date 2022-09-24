bad478c453664e7cd7ef672ebe1769b835dba3d8
a01752023j <69485255+a01752023j@users.noreply.github.com>
2022-09-24 14:31:41 -0500

    El repositorio avanzado

---
+++ app/src/main/java/mx/itesm/ajrv/proyecto/general.kt
@@ -0,0 +1,9 @@
+package mx.itesm.ajrv.proyecto
+
+import android.os.Bundle
+import mx.itesm.ajrv.proyecto.databinding.GeneralBinding
+
+class general {
+    private lateinit var binding: GeneralBinding
+
+}
