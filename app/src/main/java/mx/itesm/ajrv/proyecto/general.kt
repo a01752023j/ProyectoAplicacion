1fe9d7efc4554ba56e0c99998345c4361e707343
a01752023j <69485255+a01752023j@users.noreply.github.com>
2022-09-27 16:30:32 -0500

    hola

--- app/src/main/java/mx/itesm/ajrv/proyecto/general.kt
+++ app/src/main/java/mx/itesm/ajrv/proyecto/general.kt
@@ -1,9 +1,18 @@
 package mx.itesm.ajrv.proyecto
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import mx.itesm.ajrv.proyecto.databinding.ActivityMainBinding
 import mx.itesm.ajrv.proyecto.databinding.GeneralBinding
 
-class general {
+class general : AppCompatActivity() {
     private lateinit var binding: GeneralBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = GeneralBinding.inflate(layoutInflater)
+
+        
+    }
 
 }
